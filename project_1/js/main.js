d45b60268c573cbca3552fc7a12d4f160a6bba66
Dan <aus39360@gmail.com>
2021-02-23 22:35:36 +0200

    refactoring

--- project_1/js/main.js
+++ project_1/js/main.js
@@ -1,45 +1,47 @@
-const bread = 15.678;
-const alco = 123.965;
-const milk = 90.2345;
+const breadPrice = 15.678;
+const alcoPrice = 123.965;
+const milkPrice = 90.2345;
 
 //max
-const maxPrice = Math.max(bread, alco, milk)
+const maxPrice = Math.max(breadPrice, alcoPrice, milkPrice)
 console.log("Максимальное число:", maxPrice)
 
 //min
-const minPrice = Math.min(bread, alco, milk)
+const minPrice = Math.min(breadPrice, alcoPrice, milkPrice)
 console.log("Минимальное число:", minPrice)
 
 //sum
-const sumPrice = bread + alco + milk
+const sumPrice = breadPrice + alcoPrice + milkPrice
 console.log("Сума чисел:", sumPrice)
 
 //sum whole numbers
-const sumWholeNum = Math.floor(bread) + Math.floor(alco) + Math.floor(milk)
-console.log("Сума округленных чисел:", sumWholeNum)
+const sumRoundedPrices = Math.floor(breadPrice) + Math.floor(alcoPrice) + Math.floor(milkPrice)
+console.log("Сума округленных чисел:", sumRoundedPrices)
 
 //sum round
-const sumRound = Math.round(sumPrice/100)*100
+const sumRound = Math.round(sumPrice / 100) * 100
 console.log("Сума округленная до сотень:", sumRound)
 
 //boolean
-sumWholeNum % 2 === 0 ? console.log("парное число") : console.log("непарное число")
+sumRoundedPrices % 2 === 0 ? console.log("парное число") : console.log("непарное число")
 
 //remainder
-const remainder = 500 - sumPrice
+clientPaysUSD = 500
+const remainder = clientPaysUSD - sumPrice
 console.log("сдача с 500 грн:", remainder)
 
 //average
-const averagePrice = console.log("Средняя цена:", Number((sumPrice/3).toFixed(2)))
+const averagePrice = Number((sumPrice / 3).toFixed(2))
+console.log("Средняя цена:", averagePrice)
 
 //sale random
-const randomSale = Math.floor(Math.random()*100)
+const randomSale = Math.floor(Math.random() * 100)
 console.log("Скидка в %:", randomSale)
 
 const priceWithSale = Number((sumPrice - (sumPrice * (randomSale / 100))).toFixed(2))
 console.log("Сума к оплате с учетом скидки:", priceWithSale)
 
-const profit = Number((priceWithSale - sumPrice/2).toFixed(2))
+const profit = Number(((sumPrice / 2) - (sumPrice - priceWithSale)).toFixed(2))
 console.log("профит:", profit)
 
 //Advanced
@@ -47,7 +49,7 @@ document.writeln(`
 <p><b> Максимальное число: ${maxPrice} </b></p>
 <p><b> Минимальное число: ${minPrice} </b><p>
 <p><b> Сума чисел: ${sumPrice} </b><p>
-<p><b> Сума округленных чисел: ${sumWholeNum} </b><p>
+<p><b> Сума округленных чисел: ${sumRoundedPrices} </b><p>
 <p><b> Сума округленная до сотень: ${sumRound} </b><p>
 <p><b> сдача с 500 грн: ${remainder} </b><p>
 <p><b> Скидка в %: ${randomSale} </b><p>
