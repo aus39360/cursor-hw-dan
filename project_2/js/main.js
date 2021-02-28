321768e4273332fb1ba4b4c4435dc372f5325a14
Dan <aus39360@gmail.com>
2021-02-28 11:34:23 +0200

    hw2_ref

--- project_2/js/main.js
+++ project_2/js/main.js
@@ -16,22 +16,16 @@ const skipPairedNum = confirm('Пропускать парные числа?')
 console.log(skipPairedNum)
 
 //N + M
-/*let sum = 0
-if(skipPairedNum === true) {
-    for( i = numberN; i <= numberM; i++) {
-        if (i % 2 == 0) continue
-        sum += i 
-    }    
-} else {
-    for( i = numberN; i <= numberM; i++) {
-        sum += i 
-    } 
-}
-console.log(sum)*/
-
 let sum = 0
-for( let i = numberN; i <= numberM; i++) {
-    if (skipPairedNum && i % 2 == 0) continue
-    sum += i 
+if ( numberN <= numberM ) {
+    for( let i = numberN; i <= numberM; i++) {
+        if (skipPairedNum && i % 2 == 0) continue
+        sum += i 
+    }
+} else {
+    for( let i = numberM; i <= numberN; i++) {
+        if (skipPairedNum && i % 2 == 0) continue
+        sum += i 
+    }
 }
-console.log(sum)
+console.log('сума чисел:', sum)
